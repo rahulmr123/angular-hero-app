f90187486c709cc0f9b4c4ac58edffbed11510d5
rahulmr123 <rahulr@qburst.com>
2018-11-02 14:23:54 +0530

    added service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import {HeroService} from './hero.service';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
@@ -16,7 +16,7 @@ import { HeroDetailsComponent } from './hero-details/hero-details.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES}  from '../mock-heroes';
-
+import {HeroService} from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-heroes = HEROES;
+heroes: Hero[];
 selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    console.log(this.selectedHero);
+    // this.getHeroes();
+    this.heroes = this.heroService.getHeroes();
   }
 onSelect(hero) {
   this.selectedHero = hero;
+}
+getHeroes(): void {
+
 }
 }
